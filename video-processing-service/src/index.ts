ec5c9b21bf143591304e6b8b5462eb21506d7b24
Jakub Gładysz <jakub.glad@hotmail.com>
2025-06-13 00:27:14 +0200

    Fixed Google Cloud Configuration

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -36,7 +36,7 @@ app.post(
   }
 );
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Video processing service listening at http://localhost:${port}`);
 });
